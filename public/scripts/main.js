d76002a7b86daf759b46842c9f17f4cd757ecb8f
Jen Taylor <jen.nicole.taylor@gmail.com>
2017-02-05 13:36:50 -0500

    Changing mobile nav, adding accessibility page, fixing mobile styling on all page

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,5 +1,29 @@
-"use strict";
+'use strict';
+
+// Skip Links ---------------------
 
 window.onload = function () {
   document.getElementById("skip-link").focus();
 };
+
+// Mobile Menu --------------------
+
+$(function () {
+  var pull = $('#pull');
+  var menu = $('.menu-items ul');
+  var menuHeight = menu.height();
+  var firstLink = $('#first-link');
+
+  $(pull).on('click', function (e) {
+    e.preventDefault();
+    menu.slideToggle();
+    firstLink.focus();
+  });
+});
+
+$(window).resize(function () {
+  var windowWidth = $(window).width();
+  if (windowWidth > 320 && menu.is(':hidden')) {
+    menu.removeAttr('style');
+  }
+});
